6fb20de2f9c50aa98381bd4e8b2ae8e78723a876
Flex <70218824+wadie-2004@users.noreply.github.com>
2021-01-09 22:00:07 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-exports.Token = "NzgxNTg5NzQxNjQwMzUxNzk0.X7_2Kw.NXhSd-t-Z99ndLC6sTEhjb4Hhsg"; //Bot Token - Important
-exports.Owner = "699536220681666570"; //Bot Owner ID - Important
-exports.Default_Prefix = "."; //Bot Default Prefix (Examples: A!, !)- Important
+exports.Token = ""; //Bot Token - Important
+exports.Owner = ""; //Bot Owner ID - Important
+exports.Default_Prefix = ""; //Bot Default Prefix (Examples: A!, !)- Important
